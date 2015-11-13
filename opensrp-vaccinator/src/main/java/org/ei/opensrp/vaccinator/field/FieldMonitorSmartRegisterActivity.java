b8021bf8e0853c7e8fd95b3ab6430fb45cb88a60
muhammad.ahmed <muhammad.ahmed@ihsinformatics.com>
2015-11-13 16:56:25 +0500

    adding dialog options in FieldMonitorSmartRegisterActivity file

--- opensrp-vaccinator/src/main/java/org/ei/opensrp/vaccinator/field/FieldMonitorSmartRegisterActivity.java
+++ opensrp-vaccinator/src/main/java/org/ei/opensrp/vaccinator/field/FieldMonitorSmartRegisterActivity.java
@@ -1,12 +1,18 @@
 package org.ei.opensrp.vaccinator.field;
 
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import org.ei.opensrp.commonregistry.CommonPersonObjectController;
 import org.ei.opensrp.provider.SmartRegisterClientsProvider;
 import org.ei.opensrp.vaccinator.R;
+import org.ei.opensrp.vaccinator.child.ChildSmartClientsProvider;
 import org.ei.opensrp.view.activity.SecuredNativeSmartRegisterActivity;
+import org.ei.opensrp.view.dialog.DialogOption;
+import org.ei.opensrp.view.dialog.FilterOption;
+import org.ei.opensrp.view.dialog.ServiceModeOption;
+import org.ei.opensrp.view.dialog.SortOption;
 
 /**
  * Created by muhammad.ahmed@ihsinformatics.com on 19-Oct-15.
@@ -16,25 +22,64 @@ public class FieldMonitorSmartRegisterActivity extends SecuredNativeSmartRegiste
 
     private CommonPersonObjectController controllerByMonth;
     private CommonPersonObjectController controllerByDay;
-
+    private SmartRegisterClientsProvider clientProvider = null;
     private DefaultOptionsProvider defaultOptionProvider;
     private NavBarOptionsProvider navBarOptionsProvider;
+    private final ClientActionHandler clientActionHandler = new ClientActionHandler();
+    private boolean sortbymonth=true;
 
 
     @Override
     protected DefaultOptionsProvider getDefaultOptionsProvider() {
-        return null;
+        return new DefaultOptionsProvider() {
+            @Override
+            public ServiceModeOption serviceMode() {
+                return null;
+            }
+
+            @Override
+            public FilterOption villageFilter() {
+                return null;
+            }
+
+            @Override
+            public SortOption sortOption() {
+                return null;
+            }
+
+            @Override
+            public String nameInShortFormForTitle() {
+                return null;
+            }
+        };
     }
 
     @Override
     protected NavBarOptionsProvider getNavBarOptionsProvider() {
-        return null;
+        return new NavBarOptionsProvider() {
+            @Override
+            public DialogOption[] filterOptions() {
+                return new DialogOption[0];
+            }
+
+            @Override
+            public DialogOption[] serviceModeOptions() {
+                return new DialogOption[0];
+            }
+
+            @Override
+            public DialogOption[] sortingOptions() {
+                return new DialogOption[0];
+            }
+
+            @Override
+            public String searchHint() {
+                return null;
+            }
+        };
     }
 
-    @Override
-    protected SmartRegisterClientsProvider clientsProvider() {
-        return null;
-    }
+
 
     @Override
     protected void onInitialization() {
@@ -53,6 +98,24 @@ public class FieldMonitorSmartRegisterActivity extends SecuredNativeSmartRegiste
                 CommonPersonObjectController.ByColumnAndByDetails.byDetails.byDetails );
     }
 
+
+    @Override
+    protected SmartRegisterClientsProvider clientsProvider() {
+
+        if(sortbymonth) {
+          //  if (clientProvider == null) {
+                clientProvider = new ChildSmartClientsProvider(
+                        this, clientActionHandler, controllerByMonth, context.alertService());
+            //}
+        }
+        else{
+            clientProvider = new ChildSmartClientsProvider(
+                    this, clientActionHandler, controllerByMonth, context.alertService());
+
+
+        }
+        return clientProvider;
+    }
     @Override
     protected void startRegistration() {
 
@@ -74,6 +137,14 @@ public class FieldMonitorSmartRegisterActivity extends SecuredNativeSmartRegiste
         navBarOptionsProvider = getNavBarOptionsProvider();
 
         setupViews();
+    }
+
+
+    private class ClientActionHandler implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 
 }
