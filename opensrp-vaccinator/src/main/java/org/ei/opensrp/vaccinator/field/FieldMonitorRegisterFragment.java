7a2e5f7cba0c0d9ea697f738ea3a52515aa819b7
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2016-07-14 14:30:22 +0500

    MK: (#241) User cursor adapter for data loading

--- opensrp-vaccinator/src/main/java/org/ei/opensrp/vaccinator/field/FieldMonitorRegisterFragment.java
+++ opensrp-vaccinator/src/main/java/org/ei/opensrp/vaccinator/field/FieldMonitorRegisterFragment.java
@@ -1,68 +1,77 @@
 package org.ei.opensrp.vaccinator.field;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import org.ei.opensrp.Context;
+import org.ei.opensrp.adapter.SmartRegisterPaginatedAdapter;
 import org.ei.opensrp.commonregistry.CommonPersonObjectClient;
-import org.ei.opensrp.commonregistry.CommonPersonObjectController;
-import org.ei.opensrp.domain.form.FieldOverrides;
-import org.ei.opensrp.provider.SmartRegisterClientsProvider;
+import org.ei.opensrp.cursoradapter.CursorCommonObjectFilterOption;
+import org.ei.opensrp.cursoradapter.CursorSortOption;
+import org.ei.opensrp.cursoradapter.SmartRegisterCursorBuilder;
+import org.ei.opensrp.cursoradapter.SmartRegisterPaginatedCursorAdapter;
 import org.ei.opensrp.vaccinator.R;
 import org.ei.opensrp.vaccinator.application.common.DateSort;
 import org.ei.opensrp.vaccinator.application.common.ReportFilterOption;
-import org.ei.opensrp.vaccinator.db.Client;
-import org.ei.opensrp.vaccinator.field.FieldMonitorDailyDetailActivity;
-import org.ei.opensrp.vaccinator.field.FieldMonitorMonthlyDetailActivity;
-import org.ei.opensrp.vaccinator.field.FieldMonitorSmartClientsProvider;
-import org.ei.opensrp.vaccinator.field.StockDailyServiceModeOption;
-import org.ei.opensrp.vaccinator.field.StockMonthlyServiceModeOption;
-import org.ei.opensrp.vaccinator.application.template.DetailActivity;
-import org.ei.opensrp.vaccinator.application.template.SmartRegisterFragment;
 import org.ei.opensrp.view.activity.SecuredNativeSmartRegisterActivity;
 import org.ei.opensrp.view.controller.FormController;
-import org.ei.opensrp.view.dialog.AllClientsFilter;
+import org.ei.opensrp.view.customControls.CustomFontTextView;
 import org.ei.opensrp.view.dialog.DialogOption;
 import org.ei.opensrp.view.dialog.FilterOption;
+import org.ei.opensrp.view.dialog.SearchFilterOption;
 import org.ei.opensrp.view.dialog.ServiceModeOption;
 import org.ei.opensrp.view.dialog.SortOption;
-import org.joda.time.DateTime;
-import org.json.JSONObject;
+import org.ei.opensrp.view.fragment.SecuredNativeSmartRegisterFragment;
+import org.ei.opensrp.view.template.DetailActivity;
+import org.ei.opensrp.view.template.SmartRegisterClientsProvider;
 
 import java.util.HashMap;
-import java.util.Map;
 
-import util.Utils;
+import util.VaccinatorUtils;
+
+import static android.view.View.TEXT_ALIGNMENT_CENTER;
 
 /**
  * Created by Safwan on 2/15/2016.
  */
-public class FieldMonitorRegisterFragment extends SmartRegisterFragment {
+public class FieldMonitorRegisterFragment extends SecuredNativeSmartRegisterFragment {
+    private SmartRegisterClientsProvider clientProvider;
 
-    private FormController formController1;
     private final ClientActionHandler clientActionHandler = new ClientActionHandler();
 
+    public FieldMonitorRegisterFragment(){
+        super(null);
+    }
+
+    @SuppressLint("ValidFragment")
     public FieldMonitorRegisterFragment(FormController formController) {
         super(formController);
-        this.formController1 = formController;
     }
 
     @Override
     protected SecuredNativeSmartRegisterActivity.DefaultOptionsProvider getDefaultOptionsProvider() {
         return new SecuredNativeSmartRegisterActivity.DefaultOptionsProvider() {
+            @Override
+            public SearchFilterOption searchFilterOption() {
+                return null;
+            }
 
             @Override
             public ServiceModeOption serviceMode() {
-                return new StockDailyServiceModeOption(clientsProvider(FieldMonitorSmartClientsProvider.ByMonthByDay.ByDay));
+                return new StockDailyServiceModeOption(clientProvider);
             }
 
             @Override
             public FilterOption villageFilter() {
-                return new AllClientsFilter();
+                return new CursorCommonObjectFilterOption("no village filter", "");
             }
 
             @Override
-            public SortOption sortOption(){ return new DateSort(DateSort.ByColumnAndByDetails.byColumn, "Reporting Period", "date"); }
+            public SortOption sortOption(){ return new DateSort("Reporting Period", "date DESC"); }
 
             @Override
             public String nameInShortFormForTitle() {
@@ -83,15 +92,15 @@ public class FieldMonitorRegisterFragment extends SmartRegisterFragment {
             @Override
             public DialogOption[] serviceModeOptions() {
                 return new DialogOption[]{
-                        new StockDailyServiceModeOption(clientsProvider(FieldMonitorSmartClientsProvider.ByMonthByDay.ByDay)),
-                        new StockMonthlyServiceModeOption(clientsProvider(FieldMonitorSmartClientsProvider.ByMonthByDay.ByMonth))
+                        new StockDailyServiceModeOption(null),
+                        new StockMonthlyServiceModeOption(null)
                 };
             }
 
             @Override
             public DialogOption[] sortingOptions() {
                 return new DialogOption[]{
-                        new DateSort(DateSort.ByColumnAndByDetails.byDetails, "Reporting Period", "date")};
+                        new DateSort("Reporting Period", "date DESC")};
             }
 
             @Override
@@ -104,50 +113,65 @@ public class FieldMonitorRegisterFragment extends SmartRegisterFragment {
     @Override
     protected void onServiceModeSelection(ServiceModeOption serviceModeOption, View view) {
         super.onServiceModeSelection(serviceModeOption, view);
-        updateSearchView();
+
+        add2ndColumnHeaderView(serviceModeOption);
+    }
+
+    protected void add2ndColumnHeaderView(ServiceModeOption serviceModeOption) {
+        if (serviceModeOption.name().toLowerCase().contains("dail")){
+            LinearLayout cl = (LinearLayout) mView.findViewById(org.ei.opensrp.R.id.clients_upper_header_layout);
+            cl.setVisibility(View.VISIBLE);
+            cl.removeAllViewsInLayout();
+
+            cl.addView(addHeaderItem("", 2));
+            cl.addView(addHeaderItem("Vaccine Stock Used or Consumed over the Period", 7));
+            cl.addView(addHeaderItem("TOTAL", 2));
+        }
+    }
+
+    private CustomFontTextView addHeaderItem(String text, int weight){
+        CustomFontTextView header = new CustomFontTextView(getActivity(), null, org.ei.opensrp.R.style.CustomFontTextViewStyle_ListView_Medium);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, weight);
+        lp.gravity = Gravity.CENTER_HORIZONTAL;
+        lp.setMargins(3,0,0,0);
+        header.setLayoutParams(lp);
+        header.setGravity(TEXT_ALIGNMENT_CENTER);
+        header.setText(text);
+        header.setPadding(weight*3,0,0,1);
+        header.setTextSize(21);
+        header.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+        header.setBackgroundColor(Color.LTGRAY);
+
+        return header;
     }
 
     @Override
     protected SmartRegisterClientsProvider clientsProvider() {
         if(getCurrentServiceModeOption() == null || getCurrentServiceModeOption().name().toLowerCase().contains("daily")) {
-            return clientsProvider(FieldMonitorSmartClientsProvider.ByMonthByDay.ByDay);
+            clientProvider = clientsProvider(FieldMonitorSmartClientsProvider.ByMonthByDay.ByDay);
         }
         else {
-            return clientsProvider(FieldMonitorSmartClientsProvider.ByMonthByDay.ByMonth);
+            clientProvider =  clientsProvider(FieldMonitorSmartClientsProvider.ByMonthByDay.ByMonth);
         }
+        return clientProvider;
     }
 
     private SmartRegisterClientsProvider clientsProvider(FieldMonitorSmartClientsProvider.ByMonthByDay type) {
-        FieldMonitorSmartClientsProvider clientProvider;
-        CommonPersonObjectController controller = null;
         if (type.equals(FieldMonitorSmartClientsProvider.ByMonthByDay.ByMonth)) {
-            setCurrentSearchFilter(new ReportFilterOption("monthly"));
-            controller = new CommonPersonObjectController(context.allCommonsRepositoryobjects("stock"),
-                    context.allBeneficiaries(), context.listCache(),
-                    context.personObjectClientsCache(), "date", "stock", "report", "monthly",
-                    CommonPersonObjectController.ByColumnAndByDetails.byColumn, "date",
-                    CommonPersonObjectController.ByColumnAndByDetails.byColumn);
+            setCurrentSearchFilter(new ReportFilterOption(FieldMonitorSmartClientsProvider.ByMonthByDay.ByMonth, ""));
             clientProvider = new FieldMonitorSmartClientsProvider(
-                    getActivity().getApplicationContext(), clientActionHandler, controller, context.alertService(), FieldMonitorSmartClientsProvider.ByMonthByDay.ByMonth, context, this);
+                    getActivity().getApplicationContext(), clientActionHandler, context.alertService(), FieldMonitorSmartClientsProvider.ByMonthByDay.ByMonth);
         }
         else {
-            setCurrentSearchFilter(new ReportFilterOption("daily"));
-            controller = new CommonPersonObjectController(context.allCommonsRepositoryobjects("stock"),
-                    context.allBeneficiaries(), context.listCache(),
-                    context.personObjectClientsCache(), "date", "stock", "report", "daily",
-                    CommonPersonObjectController.ByColumnAndByDetails.byColumn, "date",
-                    CommonPersonObjectController.ByColumnAndByDetails.byColumn);
+            setCurrentSearchFilter(new ReportFilterOption(FieldMonitorSmartClientsProvider.ByMonthByDay.ByDay, ""));
             clientProvider = new FieldMonitorSmartClientsProvider(
-                    getActivity().getApplicationContext(), clientActionHandler, controller, context.alertService(), FieldMonitorSmartClientsProvider.ByMonthByDay.ByDay, context, this);
+                    getActivity().getApplicationContext(), clientActionHandler, context.alertService(), FieldMonitorSmartClientsProvider.ByMonthByDay.ByDay);
         }
         return clientProvider;
     }
 
     @Override
     protected void onInitialization() {
-        ((TextView)mView.findViewById(org.ei.opensrp.R.id.txt_title_label)).setText(getRegisterLabel());
-
-        mView.findViewById(org.ei.opensrp.R.id.btn_report_month).setVisibility(View.GONE);
 
         mView.findViewById(org.ei.opensrp.R.id.filter_selection).setVisibility(View.GONE);
 
@@ -158,43 +182,33 @@ public class FieldMonitorRegisterFragment extends SmartRegisterFragment {
         mView.findViewById(org.ei.opensrp.R.id.sorted_by).setVisibility(View.GONE);
 
         mView.findViewById(org.ei.opensrp.R.id.label_sorted_by).setVisibility(View.GONE);
-
-        ((TextView)mView.findViewById(org.ei.opensrp.R.id.statusbar_today)).setText(Utils.convertDateFormat(DateTime.now()));
     }
 
     @Override
     protected void startRegistration() {
         HashMap<String, String> overrides = new HashMap<>();
-        overrides.putAll(providerOverrides());
-        formController1.startFormActivity(getRegistrationForm(overrides), null, new FieldOverrides(new JSONObject(overrides).toString()).getJSONString());
+        overrides.putAll(VaccinatorUtils.providerDetails());
+        startForm(getRegistrationForm(overrides), "", overrides);
     }
 
-    @Override
-    protected String getRegisterLabel() {
-        return "Stock Register";
-    }
-
-    @Override
     protected String getRegistrationForm(HashMap<String, String> overridemap) {
         return "vaccine_stock_position";
-    }
-
-    @Override
-    protected String getOAFollowupForm(Client client, HashMap<String, String> overridemap) {
-        return null;
-    }
-
-    @Override
-    protected Map<String, String> customFieldOverrides() {
-        return null;
     }
 
     @Override
     protected void onResumption() {
         super.onResumption();
         getDefaultOptionsProvider();
-        updateSearchView();
+
+        add2ndColumnHeaderView(getCurrentServiceModeOption());
     }//end of method
+
+    @Override
+    protected SmartRegisterPaginatedAdapter adapter() {
+        return new SmartRegisterPaginatedCursorAdapter(getActivity(),
+                new SmartRegisterCursorBuilder("stock", getCurrentServiceModeOption().name().toLowerCase().contains("dail")?"report='daily'":"report='monthly'", (CursorSortOption) getDefaultOptionsProvider().sortOption())
+                , clientsProvider());
+    }
 
     @Override
     protected void onCreation() {    }
@@ -213,7 +227,7 @@ public class FieldMonitorRegisterFragment extends SmartRegisterFragment {
 
                     getActivity().finish();
 
-                break;
+                    break;
             }
         }
     }
