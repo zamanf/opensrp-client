71bf0320b56f4c0f7cfc2be724cb263e9fd9aa65
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2016-07-15 20:37:36 +0500

    MK: Making UI precreated instead of on load

--- opensrp-vaccinator/src/main/java/org/ei/opensrp/vaccinator/field/StockMonthlyServiceModeOption.java
+++ opensrp-vaccinator/src/main/java/org/ei/opensrp/vaccinator/field/StockMonthlyServiceModeOption.java
@@ -25,12 +25,12 @@ public class StockMonthlyServiceModeOption extends ServiceModeOption {
 
             @Override
             public int weightSum() {
-                return 12;
+                return 6;
             }
 
             @Override
             public int[] weights() {
-                return new int[]{2,2,2,2,2,2};
+                return new int[]{1,1,1,1,1,1};
             }
 
             @Override
