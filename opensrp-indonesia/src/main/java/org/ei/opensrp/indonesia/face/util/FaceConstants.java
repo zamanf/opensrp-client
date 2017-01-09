908b3c78382d871883a50d05523559abb5150aa0
wildan.mbp <wildan.ubg@gmail.com>
2017-01-09 13:33:02 +0800

    remove folder face

--- opensrp-indonesia/src/main/java/org/ei/opensrp/indonesia/face/util/FaceConstants.java
+++
@@ -1,8 +0,0 @@
-package org.ei.opensrp.indonesia.face.util;
-
-/**
- * Created by wildan on 1/4/17.
- */
-public class FaceConstants {
-    public static final int THUMBSIZE = 128;
-}
--- opensrp-indonesia/src/main/java/org/ei/opensrp/indonesia/fr/PhotoLiveConstant.java
+++
@@ -1,9 +0,0 @@
-package org.ei.opensrp.indonesia.fr;
-
-/**
- * Created by wildan on 12/22/16.
- */
-public class PhotoLiveConstant {
-    public static final String ALBUM_NAME = "serialize_deserialize";
-    public static final int CONFIDENCE_VALUE = 58;
-}
