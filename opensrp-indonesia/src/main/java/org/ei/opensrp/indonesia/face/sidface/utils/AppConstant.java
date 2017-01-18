288165823254fe25cf81cf020e57a26ed6fbec26
wildan.mbp <wildan.ubg@gmail.com>
2017-01-18 19:49:53 +0800

    remove dir face sid

--- opensrp-indonesia/src/main/java/org/ei/opensrp/indonesia/face/sidface/utils/AppConstant.java
+++
@@ -1,9 +0,0 @@
-package org.ei.opensrp.indonesia.face.sidface.utils;
-
-/**
- * Created by wildan on 12/18/16.
- */
-public class AppConstant {
-    public static final String HASH_NAME = "HashMap";
-    public static final String ALBUM_NAME = "serialize_deserialize";
-}
