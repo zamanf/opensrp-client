3c2dbaf98c99d1e7becc81275ddc413edd335fac
Ephraim Muhia <emuhia@ona.io>
2017-01-31 15:51:26 +0300

    reset couchdb config
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- opensrp-app/src/main/java/org/ei/opensrp/AllConstants.java
+++ opensrp-app/src/main/java/org/ei/opensrp/AllConstants.java
@@ -336,9 +336,9 @@ public class AllConstants {
         public static final String DOCUMENTS_REPLICATED = "DOCUMENTS_REPLICATED";
         public static final String BATCHES_REPLICATED = "BATCHES_REPLICATED";
         public static final String COUCHDB_PORT = "5984";
-        public static final String COUCH_DATABASE_NAME = "test_db1";
-        public static final String COUCH_DATABASE_USER = "rootuser";
-        public static final String COUCH_DATABASE_PASS = "adminpass";
+        public static final String COUCH_DATABASE_NAME = "test_db";
+        public static final String COUCH_DATABASE_USER = "";
+        public static final String COUCH_DATABASE_PASS = "";
 
 
 
