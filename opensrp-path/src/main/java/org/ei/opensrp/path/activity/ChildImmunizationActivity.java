12cbeed671ed8adc6c9dc8a1bfd912ca0f9f1a41
Jason Rogena <jasonrogena@gmail.com>
2017-02-16 17:51:35 +0300

    JR| Child Immunization View
    Add skeleton views for the child immunization activity
    
    Fix for issue #383
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- opensrp-path/src/main/java/org/ei/opensrp/path/activity/ChildImmunizationActivity.java
+++ opensrp-path/src/main/java/org/ei/opensrp/path/activity/ChildImmunizationActivity.java
@@ -1,6 +1,10 @@
 package org.ei.opensrp.path.activity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import org.ei.opensrp.path.R;
 
@@ -10,16 +14,60 @@ import org.ei.opensrp.path.R;
 
 public class ChildImmunizationActivity extends BaseActivity {
 
+    private static enum Gender {
+        MALE,
+        FEMALE
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getToolbar().setTitle(getActivityTitle());
+        setTitle(getActivityTitle());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getToolbar().setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChildImmunizationActivity.this.finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateGenderViews(Gender.MALE);
     }
 
     private String getActivityTitle() {
         //TODO: get the child's name
-        return String.format("? > ?", getString(R.string.app_name), "Child Name");
+        return String.format("%s > %s", getString(R.string.app_name), "Child Name");
+    }
+
+    /**
+     * Updates all gender affected views
+     */
+    private void updateGenderViews(Gender gender) {
+        int darkShade = R.color.silver;
+        int lightSade = R.color.white;
+        String identifier = getString(R.string.neutral_sex_id);
+
+        if (gender.equals(Gender.FEMALE)) {
+            darkShade = R.color.female_pink;
+            lightSade = R.color.female_light_pink;
+            identifier = getString(R.string.female_sex_id);
+        } else if (gender.equals(Gender.MALE)) {
+            darkShade = R.color.male_blue;
+            lightSade = R.color.male_light_blue;
+            identifier = getString(R.string.male_sex_id);
+        }
+
+        getToolbar().setBackground(new ColorDrawable(getResources().getColor(darkShade)));
+        ScrollView contentBase = (ScrollView) findViewById(R.id.content_base);
+        contentBase.setBackground(new ColorDrawable(getResources().getColor(lightSade)));
+        TextView childSiblingsTV = (TextView) findViewById(R.id.child_siblings_tv);
+        childSiblingsTV.setText(
+                String.format(getString(R.string.child_siblings), identifier).toUpperCase());
     }
 
     @Override
